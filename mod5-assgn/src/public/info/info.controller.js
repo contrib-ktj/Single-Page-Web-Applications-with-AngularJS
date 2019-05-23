b879e3959b2a73f2f87fcff986779e3f6cd426ca
KTJ <kshtjkmr10@gmail.comgit config --global user.name KTJq>
2019-05-23 07:03:10 +0530

    Minor fixes and update

--- mod5-assgn/src/public/info/info.controller.js
+++ mod5-assgn/src/public/info/info.controller.js
@@ -6,18 +6,18 @@
 
     infoController.$inject = ['MenuService', 'ApiPath'];
 
-    function infoController(MenuService, ApiPath) {
-        var infoCtrl = this;
-        infoCtrl.apiPath = ApiPath;
+    function infoController (MenuService, ApiPath) {
+        var $infoCtrl = this;
+        $infoCtrl.apiPath = ApiPath;
 
-        infoCtrl.signedUp = false;
+        $infoCtrl.signedUp = false;
 
-        infoCtrl.user = MenuService.getUser();
-        //console.log('User is', infoCtrl.user);
-        if (angular.equals(infoCtrl.user, {})) {
-            infoCtrl.signedUp = false;
+        $infoCtrl.user = MenuService.getUser();
+        //console.log('User is', $infoCtrl.user);
+        if (angular.equals($infoCtrl.user, {})) {
+            $infoCtrl.signedUp = false;
         } else {
-            infoCtrl.signedUp = true;
+            $infoCtrl.signedUp = true;
         }
     };
 })();
